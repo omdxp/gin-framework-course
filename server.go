cefa508e22f2daf0567664a9486c703f84d022aa
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-04 21:38:01 +0100

    get and post videos

--- server.go
+++ server.go
@@ -1,15 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Omar-Belghaouti/gin-framework-course/controller"
+	"github.com/Omar-Belghaouti/gin-framework-course/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
+	server.GET("/videos", func(c *gin.Context) {
+		c.JSON(200, videoController.FindAll())
 	})
 
-	server.Run()
+	server.POST("/videos", func(c *gin.Context) {
+		c.JSON(200, videoController.Save(c))
+	})
+
+	server.Run(":8080")
 }
